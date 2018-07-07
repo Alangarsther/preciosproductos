b98d89dc91eb450874de22a16e160cea74a054db
Alan Garcia <alan_gh89@hotmail.com>
2018-07-07 12:50:18 -0700

    Comentario test

--- PU2/PU2/Form1.cs
+++ PU2/PU2/Form1.cs
@@ -16,6 +16,7 @@ namespace PU2
         public Form1()
         {
             InitializeComponent();
+            //comentario test
         }
 
         private void Form1_Load(object sender, EventArgs e)
