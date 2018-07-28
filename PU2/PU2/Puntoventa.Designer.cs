2cfd19c3bcb335bc06d3757d783400158b84fc4c
Alan Garcia <alan_gh89@hotmail.com>
2018-07-28 14:52:26 -0700

    seguimiento4

--- PU2/PU2/Puntoventa.Designer.cs
+++ PU2/PU2/Puntoventa.Designer.cs
@@ -34,6 +34,9 @@
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,11 +75,17 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(443, 165);
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(495, 164);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(305, 150);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.TabStop = false;
             // 
             // textBox1
             // 
@@ -84,7 +93,8 @@
             this.textBox1.Location = new System.Drawing.Point(418, 410);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 38);
-            this.textBox1.TabIndex = 4;
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // label4
             // 
@@ -96,6 +106,21 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "TOTAL";
             this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Cantidad";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "producto";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Total";
+            this.Column3.Name = "Column3";
             // 
             // Puntoventa
             // 
@@ -129,5 +154,8 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
--- PU2/PU2/Puntoventa.cs
+++ PU2/PU2/Puntoventa.cs
@@ -12,6 +12,9 @@ namespace PU2
 {
     public partial class Puntoventa : Form
     {
+        private double total = 0;
+        bool existe=false;
+        private string[] cantidadproducto;
         public Puntoventa()
         {
             InitializeComponent();
@@ -25,15 +28,103 @@ namespace PU2
             label3.Location = new Point(this.Width / 2 - label3.Width / 2, label1.Height + label2.Height + 21);
             dataGridView1.Width = this.Width - 3;
             dataGridView1.Height = int.Parse(this.Height * 075f + "");
-            dataGridView1.Location = new Point(1, label1.Height + label2.Height +  label3.Height + 21);
+            dataGridView1.Location = new Point(1, label1.Height + label2.Height + label3.Height + 21);
             textBox1.Width = this.Width - 2;
             textBox1.Location = new Point(0, this.Height - textBox1.Height);
+
+            dataGridView1.Columns[0].Width = Convert.ToInt32(this.Width * 0.15);
+            dataGridView1.Columns[1].Width = Convert.ToInt32(this.Width * 0.645);
+            dataGridView1.Columns[0].Width = Convert.ToInt32(this.Width * 0.20);
             label4.Location = new Point(this.Width - label4.Width + 2, this.Height - textBox1.Height - label4.Height);
+            updateFontDgv();
         }
 
         private void label4_Click(object sender, EventArgs e)
         {
 
         }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                //MessageBox.Show(textBox1.Text);
+                if (textBox1.Text.IndexOf("*")==-1)
+                {
+                    buscarproducto(1,textBox1.Text);
+                }
+                else
+                {
+                    cantidadproducto=textBox1.Text.Split('*');
+                    buscarproducto(int.Parse(cantidadproducto[0]), cantidadproducto[1]);
+                }
+                
+                textBox1.Clear();
+                textBox1.Focus();
+            }
+        }
+        private void buscarproducto(int cantidad,string idproducto)
+        {
+            string[] infoproducto;
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\AlanGarcia\source\repos\PU2\PU2\bin\Debug\precios y productos.csv");
+            MessageBox.Show(idproducto);
+            while ((line = file.ReadLine()) != null)
+            {
+                infoproducto = line.Split(',');
+                MessageBox.Show(idproducto + " " + infoproducto[0]);
+                if (idproducto == infoproducto[0])
+                {
+                    dataGridView1.Rows.Add(cantidad, infoproducto[1], infoproducto[2], cantidad * double.Parse(infoproducto[2]));
+                    total = total + Convert.ToDouble(infoproducto[2]);
+                   
+                    existe = true;
+
+                    //label4.Text = "Nombre" + precioproducto[1] + "Precio:$" + precioproducto[2];
+                }
+            }
+            file.Close();
+        }
+        private void updateFontDgv()
+        {
+            foreach ( DataGridViewColumn c in dataGridView1.Columns)
+            {
+                c.HeaderCell.Style.Font = new Font("Arial", 20, GraphicsUnit.Pixel);
+            }
+            dataGridView1.Columns[0].Width = Convert.ToInt32(this.Width * 0.15);
+            dataGridView1.Columns[1].Width = Convert.ToInt32(this.Width * 0.645);
+            dataGridView1.Columns[0].Width = Convert.ToInt32(this.Width * 0.20);
+        }
+        private void Eliminarproducto()
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                dataGridView1.Rows.RemoveAt(dataGridView1.Rows.Count - 1);
+            }
+           
+        }
+        private void Total()
+        {
+            double total = 0;
+            for (int i = 0; i <dataGridView1.Rows.Count;i++)
+            {
+                total += Convert.ToDouble(dataGridView1[3, i].Value.ToString());
+
+            }
+            label4.Text = "total$:" + total.ToString("n");
+            label4.Location = new Point(this.Width - label4.Width + 2, this.Height - textBox1.Height - label4.Height);
+
+        }
+        private void dublicarultimoproducto()
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                dataGridView1.Rows.Add(dataGridView1[0,dataGridView1.Rows.Count-1].Value.ToString());
+                dataGridView1[1, dataGridView1.Rows.Count - 1].Value.ToString();
+                dataGridView1[2, dataGridView1.Rows.Count - 1].Value.ToString();
+                dataGridView1[3, dataGridView1.Rows.Count - 1].Value.ToString();
+            }
+        }
+
     }
 }
